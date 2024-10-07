782480693974a3d14d7420044caa508e9b2b9d3f
ymolins <2024_yahel.molins@iticbcn.cat>
2024-10-07 16:06:48 +0200

    modificar convert.js

--- convert.js
+++ convert.js
@@ -2,12 +2,13 @@ function convertirCF() {
     let CF = prompt("A què vols convertir C/F?");
     let temp = parseFloat(prompt("Temperatura:"));
     if( CF.toUpperCase() == "C" ){
-        let cent = (temp-32)/(9.0/5.0);
+        let cent = (temp-32)/(5/9);
         console.log(`${temp}F -> ${cent}C`);
     } else if (CF.toUpperCase() == "F"){
-        let fh = (9.0/5.0) * temp + 32;
+        let fh = (5/9) * temp + 32;
         console.log(`${temp}C -> ${fh}F`);
     } else {
         console.log("Només es pot C ó F");
     }
 }
+
